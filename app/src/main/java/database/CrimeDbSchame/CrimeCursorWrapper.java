71d2972d118d2fde149bd87a0727d709a797fc51
surimj <suri_mj@163.com>
2016-12-15 23:23:54 +0800

    SQLite finally

---
+++ app/src/main/java/database/CrimeDbSchame/CrimeCursorWrapper.java
@@ -0,0 +1,10 @@
+package database.CrimeDbSchame;
+
+import android.database.CursorWrapper;
+
+/**
+ * Created by dragyu on 2016/12/15.
+ */
+
+public class CrimeCursorWrapper extends CursorWrapper {
+}
